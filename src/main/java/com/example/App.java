406dcf728f7c371b73ad2952def4de14e634a604
c4diaz <CARLOS DIAZ@DESKTOP-91AU5N9>
2024-05-28 12:38:31 +0200

    Rama 1 sufijo F para un valor float

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!" );
+        
+        
+        //Tipo de datos primitivos: No tiene propiedades ni metodos: ejemplo
+        int a = 10;
+        
+        //Los tipos de datos objetos que son un envoltorio de los tipos primitivos
+        // si tienen propiedades y metodos. Por cada tipo primitivo existe un tipo objeto correspondiente.
+        
+        Integer b = 20;
+        b.getClass();
+        
+        //para almacenar un valor en un float, hay que especificar un sufijo (f oF) por que de lo contrario
+        //considera el valor como un double, y un double no se puede almacenar en un flotante.
+        
+        //"el double es mas grande que un float"
+        
+        float c = 2.3f;
+        //si no ponemos la f , hacemos un casteo = float d = (float) 2.3;
+        
+        //Tipo de dato : Array
     }
 }
